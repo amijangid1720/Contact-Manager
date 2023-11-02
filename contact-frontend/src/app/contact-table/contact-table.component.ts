dc5f8ab69dac07167ecc99f2b57e20e7c0176c48
Manisha <mkumari@argusoft.com>
2023-11-02 11:44:21 +0530

    display all contacts implemented

--- contact-frontend/src/app/contact-table/contact-table.component.ts
+++ contact-frontend/src/app/contact-table/contact-table.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { faUserPen,faTrashCan,faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { HttpClient } from '@angular/common/http';
+// import { map } from 'rxjs';
+// import { contacts } from '../model/contacts';
+import { ManipulateUserService } from '../service/manipulate-user.service';
 
 @Component({
   selector: 'app-contact-table',
   templateUrl: './contact-table.component.html',
-  styleUrls: ['./contact-table.component.scss']
+  styleUrls: ['./contact-table.component.scss'],
 })
-export class ContactTableComponent {
+export class ContactTableComponent  implements OnInit{
   faUserPen = faUserPen;
   faTrashCan = faTrashCan;
   faArrowRightFromBracket = faArrowRightFromBracket;
+  contacts!: any[];
+
+  constructor(private http:HttpClient,
+    private manipulateuser: ManipulateUserService) { }
+
+  ngOnInit() {
+    const yourAuthToken = localStorage.getItem('token');
+    if(yourAuthToken !=null){
+    this.manipulateuser.getContacts(yourAuthToken).subscribe((data:any[])=>{
+      console.log(data);
+      
+      this.contacts = data.map(contact => ({
+        name: contact.firstname + ' ' + contact.lastname,
+        email: contact.email,
+        phoneno: contact.phoneno,
+      }));
+    });
+  }
+  else{
+    console.log("token not found");
+    
+  }
+  }
+
 }
--- contact-frontend/src/app/service/manipulate-user.service.ts
+++ contact-frontend/src/app/service/manipulate-user.service.ts
@@ -1,21 +1,41 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Injectable, OnInit } from '@angular/core';
+import { Observable, map } from 'rxjs';
+// import { contacts } from '../model/contacts';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ManipulateUserService {
+export class ManipulateUserService implements OnInit { 
 
+  // allContacts: contacts[] = [];
   constructor(private http:HttpClient) { }
-
-  getUserName(token:string, email:string): Observable<any>
-  {
-      const headers = new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`,
-      });
-        return this.http.get(`http:localhost:8082/api/v1/get-name/${email}`, { headers });
-        // `http:localhost:8082/api/v1/get-name/:email`
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
+  
+  // getUserName(token:string, email:string): Observable<any>
+  // // {
+  //     const headers = new HttpHeaders({
+  //       'Content-Type': 'application/json',
+  //       'Authorization': `Bearer ${token}`,
+  //     });
+  //       return this.http.get(`http:localhost:8082/api/v1/get-name/${email}`, { headers });
+  //       // `http:localhost:8082/api/v1/get-name/:email`
+  // }
+
+
+   getContacts(yourAuthToken:string):Observable<any> {
+    const headers = new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${yourAuthToken}`,
+          });
+
+        
+    return this.http.get(`http://localhost:8082/api/v1/contacts/findAll`, {headers});
+
+   }
+ 
 }
+
+ // const options = {headers:headers}
